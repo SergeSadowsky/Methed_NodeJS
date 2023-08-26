3ca7e470c85f9849f77fd3a635368b0343494bbc
uncleserge <serge@sadowsky.spb.ru>
2023-08-26 15:38:20 +0300

    t2 finished

---
+++ t2/index.js
@@ -0,0 +1,9 @@
+const mypackage = require('mypackage');
+
+const result = mypackage.myFunc({    
+    name: 'максим лескин',
+    dateBirth: "10.03.1987",
+    purpose: 'карьерный рост'
+});
+
+console.log('result: ', result);
